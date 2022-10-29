f2ac4a4bccdaea62f7a3e7e6c75c4e22074ebae4
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-29 18:39:58 -0300

    lesson 25

--- MODULO2-BASICS E CORE CONCEPTS - INTERAGINDOCOMODOM,USANDOVUE/NOVOPROJETO6-DYNAMICSTYLING/app.js
+++ MODULO2-BASICS E CORE CONCEPTS - INTERAGINDOCOMODOM,USANDOVUE/NOVOPROJETO6-DYNAMICSTYLING/app.js
@@ -1,13 +1,31 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
   },
 
   watch: {},
 
   computed: {},
 
-  methods: {},
+  methods: {
+    toggleStyle(letter) {
+      switch (letter) {
+        case 'A':
+          this.boxASelected = !this.boxASelected;
+          break;
+        case 'B':
+          this.boxBSelected = !this.boxBSelected;
+          break;
+        case 'C':
+          this.boxCSelected = !this.boxCSelected;
+          break;
+      }
+    },
+  },
 });
 
-app.mount('#styling');
+app.mount('#style');
