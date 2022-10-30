3154db8a4bf6f6d576ccea7bed8bcdca9f4fbae2
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-10-30 11:16:12 -0300

    lesson 3 finished

--- MODULO3-RENDERCONDICIONALDECONTEUDOELISTS/novoprojetoLists1/app.js
+++ MODULO3-RENDERCONDICIONALDECONTEUDOELISTS/novoprojetoLists1/app.js
@@ -2,11 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       goals: [],
+      userInput: '',
     };
   },
   watch: {},
   computed: {},
-  methods: {},
+  methods: {
+
+
+
+
+    addGoal() {
+      this.goals.push(this.userInput);
+    },
+  },
 });
 
 app.mount('#user-goals');
