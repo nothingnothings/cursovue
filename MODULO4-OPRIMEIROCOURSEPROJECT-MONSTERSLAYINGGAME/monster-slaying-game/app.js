f3da1f30f4204bf2e04c6dce67b1313e0788e712
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-11-02 13:29:51 -0300

    improved monster slaying game

--- MODULO4-OPRIMEIROCOURSEPROJECT-MONSTERSLAYINGGAME/monster-slaying-game/app.js
+++ MODULO4-OPRIMEIROCOURSEPROJECT-MONSTERSLAYINGGAME/monster-slaying-game/app.js
@@ -1,3 +1,11 @@
+const isEven = (value) => {
+  if (value % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const app = Vue.createApp({
   data() {
     return {
@@ -5,6 +13,8 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       battleStatus: null,
+      round: 0,
+      canSpecialAttack: false,
     };
   },
   watch: {
@@ -17,40 +27,60 @@ const app = Vue.createApp({
     monsterHealth() {
       if (this.monsterHealth < 0 && this.playerHealth > 0) {
         this.battleStatus = 'won!';
-        
       }
+    },
+
+    round(newWatchedValue, oldWatchedValue) {
+
+      console.log(isEven(newWatchedValue), isEven(oldWatchedValue), 'VALUES');
+      if (
+        this.round === 3
+      ) {
+        this.canSpecialAttack = true;
+      } else {
+        this.canSpecialAttack = false;
+      }
+      console.log(this.round);
     },
   },
   computed: {
-    monsterHealthString() {
-      return this.monsterHealth.toString() + '%';
+    // monsterHealthString() {
+    //   return this.monsterHealth.toString() + '%';
+    // },
+
+    // playerHealthString() {
+    //   return this.playerHealth.toString() + '%';
+    // },
+
+    ////ainda maior outsourcing de código (tiramos a lógica lá do html, para colocar nesse app vue, javascript)...
+    monsterBarStyles() {
+      return { width: this.monsterHealth.toString() + '%' };
     },
 
-    playerHealthString() {
-      return this.playerHealth.toString() + '%';
+    playerBarStyles() {
+      return { width: this.playerHealth.toString() + '%' };
     },
   },
   methods: {
     attack() {
-
       const playerDamage = Math.random().toFixed(2) * 10;
       const monsterDamage = Math.random().toFixed(2) * 10;
       this.playerHealth = this.playerHealth - playerDamage;
       this.monsterHealth = this.monsterHealth - monsterDamage;
-
+      if (this.round !== 3) {
+        this.round += 1;
+      }
       this.logFight(playerDamage, monsterDamage);
-
-
     },
 
     specialAttack() {
-
       const playerDamage = Math.random().toFixed(2) * 10;
       const monsterDamage = Math.random().toFixed(2) * 20;
 
       this.playerHealth = this.playerHealth - playerDamage;
       this.monsterHealth = this.monsterHealth - monsterDamage;
-
+      this.canSpecialAttack = !this.canSpecialAttack;
+      this.round = 0;
       this.logFight(monsterDamage, playerDamage);
     },
 
@@ -58,6 +88,9 @@ const app = Vue.createApp({
       const healAmount = Math.random() * 10;
       if (this.playerHealth < 100 && this.playerHealth + healAmount < 100) {
         this.playerHealth = this.playerHealth + healAmount;
+        if (this.round !== 3) {
+          this.round += 1;
+        }
         this.logFight(0, 0, healAmount);
       } else {
         alert('You cannot heal more than your max health.');
@@ -68,6 +101,7 @@ const app = Vue.createApp({
       if (answer) {
         this.battleStatus = 'lost!';
         this.log = [];
+        this.round = 0;
       } else {
         return;
       }
@@ -76,19 +110,16 @@ const app = Vue.createApp({
       this.battleStatus = null;
       this.playerHealth = 100;
       this.monsterHealth = 100;
-      this.log = [];
     },
 
-    logFight(playerDamage, monsterDamage, amountHealed=0) {
-      this.log.push(
-        {
-          count: Math.random(),
-          playerDamage: playerDamage,
-          monsterDamage: monsterDamage,
-          amountHealed: amountHealed
-        }
-      )
-    }
+    logFight(playerDamage, monsterDamage, amountHealed = 0) {
+      this.log.push({
+        count: Math.random(),
+        playerDamage: playerDamage,
+        monsterDamage: monsterDamage,
+        amountHealed: amountHealed,
+      });
+    },
   },
 });
 app.mount('.game');
