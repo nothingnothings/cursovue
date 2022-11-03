568ada9bb09c7af1fd89b615cfbff85166a787d8
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-11-03 00:00:13 -0300

    lesson 7 finished

--- MODULO5-VUEPORTRÁSDASCENAS/novoProjeto-exemploMultiplosApps/app.js
+++ MODULO5-VUEPORTRÁSDASCENAS/novoProjeto-exemploMultiplosApps/app.js
@@ -18,10 +18,7 @@ const app2 = Vue.createApp({
 });
 app.mount('#app2');
 
-
-
 const app3 = Vue.createApp({
-
   template: `
   <section id="app3">
   <input type="text" @input="saveInput">
@@ -31,13 +28,47 @@ const app3 = Vue.createApp({
   <p>{{ }}</p>
 </section>`,
   data() {
-    return {
-
-
-    };
+    return {};
   },
   watch: {},
   computed: {},
   methods: {},
+});
+app.mount();
+
+
+
+
+const app4 = Vue.createApp({
+  data() {
+    return {
+
+      currentUserInput: '',
+      message: 'Vue is great!';
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+
+    saveInput(event) {
+      this.currentUserInput = event.target.value
+    },
+
+    setText() {
+      // this.message = this.currentUserInput;
+   
+      // this.message = this.$refs ////PROPRIEDADE 'refs' é especial, é PROVIDENCIADA PELO VUE... todas as PROPRIEDADES PROVIDENCIADAS PELO VUE COMEÇAM COM 'dollar sign'...
+    
+      // this.message = this.$refs.userText ///acessamos a propriedade/referencial 'userText' QUE DEFINIMOS LÁ NO TEMPLATE/HTML 'app3', no input field..
+      
+      console.log(this.$refs.userText); ////// --> ISSO VAI NOS MOSTRAR O ELEMENTO HTML INTEIRO, o 'input element' inteiro..
+      console.dir(this.$refs.userText); //ISSO NOS MOSTRA O JAVASCRIPT OBJECT REPRESENTATION DAQUELE ELEMENT( com todas as propriedades)...
+   
+      this.message = this.$refs.userText.value; ///ACESSAMOS O 'VALUE' dentro desse input element...
+    }
+
+
+  },
 });
 app.mount();
