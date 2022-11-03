b7189d0498f02117d2644a1977b5ff254fadbf4a
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-11-02 23:37:00 -0300

    lesson 6 finished

--- MODULO5-VUEPORTRÁSDASCENAS/novoProjeto-exemploMultiplosApps/app.js
+++ MODULO5-VUEPORTRÁSDASCENAS/novoProjeto-exemploMultiplosApps/app.js
@@ -17,3 +17,27 @@ const app2 = Vue.createApp({
   methods: {},
 });
 app.mount('#app2');
+
+
+
+const app3 = Vue.createApp({
+
+  template: `
+  <section id="app3">
+  <input type="text" @input="saveInput">
+  <button @click="setText">
+      Set Text
+  </button>
+  <p>{{ }}</p>
+</section>`,
+  data() {
+    return {
+
+
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+});
+app.mount();
