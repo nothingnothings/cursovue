76cc9c22e7ef47d857b00e5aa4daa821340608d9
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-11-15 15:24:06 -0300

    finished lesson 3

--- MODULO9-MERGULHANDOFUNDOEMCOMPONENTS/novo-projeto-deep-dive-components/src/App.vue
+++ MODULO9-MERGULHANDOFUNDOEMCOMPONENTS/novo-projeto-deep-dive-components/src/App.vue
@@ -1,41 +1,47 @@
 <template>
   <the-header></the-header>
   <badge-list></badge-list>
-  <user-info
-  :full-name="activeUser.name"
-  :info-text="activeUser.description"
-  :role="activeUser.role"
-  >
+  <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role">
 
   </user-info>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 
 
-export default {  
 
-    components: ['the-header', 'badge-list', 'user-info'],
+export default {
 
-    data() {
-      return {
-        activeUser: {
-          name: 'Max Schwarz',
-          description: 'Site owner and admin',
-          role: 'admin'
-        }
+  components: {
+
+    // 'the-header': TheHeader  /////É __ ASSIM_ QUE REGISTRAMOS UM COMPONENT DE FORMA 'LOCAL' (poderá ser usado o 'the-header' APENAS NO CONTEXTO DE APP.VUE... dentro do seu template)...
+    TheHeader, ////SINTAXE SUPERIOR/MODERNA/MAIS RÁPIDA (e que te permite escrever tanto '<the-header></the-header>' como '<TheHeader>')
+    BadgeList,
+    UserInfo
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Max Schwarz',
+        description: 'Site owner and admin',
+        role: 'admin'
       }
     }
+  }
 
 
 }
 </script>
 
 <style>
-  html {
-    font-family: sans-serif;
-  }
-  body {
-    margin: 0;
-  }
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
 </style>
--- MODULO9-MERGULHANDOFUNDOEMCOMPONENTS/novo-projeto-deep-dive-components/src/main.js
+++ MODULO9-MERGULHANDOFUNDOEMCOMPONENTS/novo-projeto-deep-dive-components/src/main.js
@@ -1,15 +1,15 @@
 import { createApp } from 'vue';
-import BadgeList from './components/BadgeList.vue';
+// import BadgeList from './components/BadgeList.vue';
 import BaseBadge from './components/BaseBadge.vue';
-import TheHeader from './components/TheHeader.vue';
-import UserInfo from './components/UserInfo.vue';
+//import TheHeader from './components/TheHeader.vue';
+// import UserInfo from './components/UserInfo.vue';
 import App from './App.vue';
 
 const app = createApp(App);
 
-app.component('badge-list', BadgeList);
-app.component('base-badge', BaseBadge);
-app.component('the-header', TheHeader);
-app.component('user-info', UserInfo);
+// app.component('badge-list', BadgeList);
+app.component('base-badge', BaseBadge); ///ESTE REALMENTE É USADO GLOBALMENTE (é usado tanto em 'badge-list' como em 'user-info' )
+//app.component('the-header', TheHeader); ///REMOVEMOS ESSE IMPORT GLOBAL DE 'HEADER', PQ ELE NÃO É UTILIZADO AO LONGO DE TODO NOSSO APP (coisa redundante, desnecessária)...
+// app.component('user-info', UserInfo);
 
 app.mount('#app');
