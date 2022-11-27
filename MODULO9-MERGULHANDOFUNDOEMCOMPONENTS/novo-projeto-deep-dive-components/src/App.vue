b0dca0edc03375208d6a1e7facf92328ae974afc
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-11-26 23:58:30 -0300

    finished lesson 10

--- MODULO9-MERGULHANDOFUNDOEMCOMPONENTS/novo-projeto-deep-dive-components/src/App.vue
+++ MODULO9-MERGULHANDOFUNDOEMCOMPONENTS/novo-projeto-deep-dive-components/src/App.vue
@@ -6,10 +6,25 @@
   </user-info>
   <course-goals>
     <template #default="slotProps">
-      <h2>{{slotProps.goal}}</h2>
-      <h2>{{slotProps['another-goal']}}</h2>
+      <h2>{{ slotProps.goal }}</h2>
+      <h2>{{ slotProps['another-goal'] }}</h2>
     </template>
   </course-goals>
+  <button @click="setSelectedComponent('active-goals')">Active Goals></button>
+  <button @click="setSelectedComponent('manage-goals')">Manage Goals></button>
+
+
+  <!-- maneira tradicional de escrever components e sua renderização no vue (sem dynamic components) -->
+  <!-- <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
+  <active-goals v-if="selectedComponent === 'active-goals'"></active-goals> -->
+
+  <!-- MANEIRA MAIS MODERNA DE ESCREVER A MESMA COISA: -->
+  <!-- <component is="active-goals"></component> -->
+
+
+  <!-- USAMOS O PROP 'is' para COMUNICAR __ QUAL__ DOS NOSSOS COMPONENTS QUEREMOS RENDERIZAR NO LUGAR DESSE 'PLACEHOLDER'-->
+
+  <component :is="selectedComponent"></component>
 </template>
 
 <script>
@@ -17,6 +32,8 @@ import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
 import CourseGoals from './components/CourseGoals.vue';
+// import ActiveGoals from './components/ActiveGoals.vue';
+// import ManageGoals from './components/ManageGoals.vue';
 
 
 
@@ -28,15 +45,26 @@ export default {
     TheHeader, ////SINTAXE SUPERIOR/MODERNA/MAIS RÁPIDA (e que te permite escrever tanto '<the-header></the-header>' como '<TheHeader>')
     BadgeList,
     UserInfo,
-    CourseGoals
+    CourseGoals,
+    //PROFESSOR VAI NOS MOSTRAR UMA MANEIRA ALTERNATIVA DE ESCREVER ISSO... com os 'dynamic components'...
+    // ActiveGoals,
+    // ManageGoals
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Max Schwarz',
         description: 'Site owner and admin',
         role: 'admin'
       }
+    }
+  },
+
+
+  methods: {
+    setSelectedComponent(component) {
+        this.selectedComponent = component
     }
   }
 
