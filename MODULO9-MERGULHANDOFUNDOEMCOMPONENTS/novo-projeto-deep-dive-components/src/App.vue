789f1b9fd45eaa10725045b3533cc4cdc7c96bbb
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-11-26 22:58:12 -0300

    finished lesson 9

--- MODULO9-MERGULHANDOFUNDOEMCOMPONENTS/novo-projeto-deep-dive-components/src/App.vue
+++ MODULO9-MERGULHANDOFUNDOEMCOMPONENTS/novo-projeto-deep-dive-components/src/App.vue
@@ -4,12 +4,19 @@
   <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role">
 
   </user-info>
+  <course-goals>
+    <template #default="slotProps">
+      <h2>{{slotProps.goal}}</h2>
+      <h2>{{slotProps['another-goal']}}</h2>
+    </template>
+  </course-goals>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 
 
@@ -20,7 +27,8 @@ export default {
     // 'the-header': TheHeader  /////É __ ASSIM_ QUE REGISTRAMOS UM COMPONENT DE FORMA 'LOCAL' (poderá ser usado o 'the-header' APENAS NO CONTEXTO DE APP.VUE... dentro do seu template)...
     TheHeader, ////SINTAXE SUPERIOR/MODERNA/MAIS RÁPIDA (e que te permite escrever tanto '<the-header></the-header>' como '<TheHeader>')
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals
   },
   data() {
     return {
---
+++ MODULO9-MERGULHANDOFUNDOEMCOMPONENTS/novo-projeto-deep-dive-components/src/components/CourseGoals.vue
@@ -0,0 +1,34 @@
+
+
+
+
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :goal="goal" :another-goal="goal"></slot>
+        </li>
+    </ul>
+</template>
+
+
+<script>
+
+export default {
+
+    data() {
+        return {
+
+            goals: ['Finish the course', 'Learn Vue']
+        }
+    }
+}
+
+
+</script>
+
+
+
+
+<style>
+
+</style>
