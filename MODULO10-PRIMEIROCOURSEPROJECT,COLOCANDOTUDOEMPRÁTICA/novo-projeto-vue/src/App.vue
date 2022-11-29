fe1731cc67ab934098fdbc8892c55b558cfd5215
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-11-29 13:11:10 -0300

    finished lesson 2

---
+++ MODULO10-PRIMEIROCOURSEPROJECT,COLOCANDOTUDOEMPRÁTICA/novo-projeto-vue/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+
+  <ul>
+    <li v-for="resource in storedResources">
+
+    </li>
+  </ul>
+</template>
+
+<script>
+
+import ResourceItem from '../src/components/ResourceItem/ResourceItem'
+
+export default {
+
+
+  components: [
+    ResourceItem
+  ],
+
+  data() {
+    return {
+
+      storedResources: [
+
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official VueJS documentation.',
+          link: 'https://vuejs.org'
+        },
+
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google...',
+          link: 'https://google.com'
+        }
+
+      ]
+    }
+  },
+
+
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
