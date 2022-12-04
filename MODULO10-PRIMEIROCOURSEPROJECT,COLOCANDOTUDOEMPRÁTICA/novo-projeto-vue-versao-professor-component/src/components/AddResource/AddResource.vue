58b2a38e4cc3c8db8296d03b203ec2b3518443e2
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-12-04 15:27:26 -0300

    finished lesson 9

--- MODULO10-PRIMEIROCOURSEPROJECT,COLOCANDOTUDOEMPRÁTICA/novo-projeto-vue-versao-professor-component/src/components/AddResource/AddResource.vue
+++ MODULO10-PRIMEIROCOURSEPROJECT,COLOCANDOTUDOEMPRÁTICA/novo-projeto-vue-versao-professor-component/src/components/AddResource/AddResource.vue
@@ -2,25 +2,28 @@
 
 
 
-
-    <form>
-        <div>
-            <label for="title">Title</label>
-            <input type="text" v-model="userText" required>
-        </div>
-        <div>
-            <label for="description">Description</label>
-            <input type="text" v-model="userDescription" required>
-        </div>
-        <div>
-            <label for="link">Link</label>
-            <input type="text" v-model="userLink" required>
-        </div>
-        <base-button :type="'button'" 
-        :mode="'normal'" 
-        :disabled="userText === '' || userDescription === '' || userLink === '' "
-            @click="addResource({ title: userText, description: userDescription, link: userLink })">Add Resource</base-button>
-    </form>
+    <base-card>
+        <form>
+            <div class="form-control">
+                <label for="title">Title</label>
+                <input type="text" v-model="userText" required name="title">
+            </div>
+            <div class="form-control">
+                <label for="description">Description</label>
+                <textarea v-model="userDescription" required name="description" rows="4"></textarea>
+            </div>
+            <div class="form-control">
+                <label for="link">Link</label>
+                <input type="url" v-model="userLink" required name="link">
+            </div>
+            <div>
+                <base-button :type="'button'" :mode="'normal'"
+                    :disabled="userText === '' || userDescription === '' || userLink === ''"
+                    @click="addResource({ title: userText, description: userDescription, link: userLink })">Add
+                    Resource</base-button>
+            </div>
+        </form>
+    </base-card>
 
 </template>
 
@@ -54,5 +57,29 @@ export default {
 
 
 <style scoped>
+label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+}
 
+input,
+textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    padding: 0.15rem;
+    border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+    outline: none;
+    border-color: #3a0061;
+    background-color: #f7ebff;
+}
+
+.form-control {
+    margin: 1rem 0;
+}
 </style>
