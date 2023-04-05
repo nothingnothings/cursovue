423f3c77b22444e48e2f3759c42c32c580ffbe2e
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-04-05 00:45:13 -0300

    finished lesson 15

--- MODULO14-ANIMATIONS-E-TRANSITIONS-COM-O-VUE/projeto-animations/src/components/UsersList.vue
+++ MODULO14-ANIMATIONS-E-TRANSITIONS-COM-O-VUE/projeto-animations/src/components/UsersList.vue
@@ -2,11 +2,16 @@
 <template>
   <!-- USADO PARA EXEMPLIFICAR O USO DE ANIMATIONS, DO VUE, COM LISTS...-->
   <div>
-    <ul>
+    <!-- <ul> -->
+    <!-- <transition> .... SE FOR TRABALHAR COM LIST ITEMS DENTRO DE 1 LIST, USE 'transition-group', e não 'transition'-->
+    <transition-group tag="ul" name="user-list">
+      ><!-- COM A PROPRIEDADE 'tag=""', DEFINIMOS QUAL _ _ ELEMENTO DEVERÁ SER RENDERIZADO __ NO LUGAR DESSE 'transition-group'-->
       <li @click="removeUser(user)" v-for="user in users" :key="user">
         {{ user }}
       </li>
-    </ul>
+    </transition-group>
+    <!-- </transition> -->
+    <!-- </ul> -->
 
     <div>
       <input type="text" ref="userNameInput" />
@@ -48,5 +53,31 @@ li {
   border: 1px solid #ccc;
   padding: 1rem;
   text-align: center;
+}
+
+
+/* USADO COM 'transition-group' */
+.user-list-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.user-list-enter-active {
+  transition: all 1s ease-out;
+}
+
+.user-list-enter-to,
+.user-list-leave-from {
+  opacity: 1;
+  transform: translateX(-30px);
+}
+
+.user-list-leave-active {
+  transition: all 1s ease-in;
+}
+
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
