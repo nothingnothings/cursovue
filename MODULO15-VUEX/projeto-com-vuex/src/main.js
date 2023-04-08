39dd3abd6af8ffed2ebb9f2e8a1f6113bbfe7bc3
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-04-08 16:26:26 -0300

    finished lesson 9

--- MODULO15-VUEX/projeto-com-vuex/src/main.js
+++ MODULO15-VUEX/projeto-com-vuex/src/main.js
@@ -70,6 +70,10 @@ const store = createStore({
       setTimeout(
         () => {
           context.commit('increment')
+
+          // context.dispatch --> para dispatchar OUTRAS ACTIONS, de dentro de sua action
+          // context.getters ---> para GANHAR ACESSO AOS DERIVED VALUES DE OUTROS GETTERS
+          // context.state --> PARA GANHAR ACESSO AO VALUE DE SEU STATE (sem o mutar diretamente)...
         },
 
         2000
