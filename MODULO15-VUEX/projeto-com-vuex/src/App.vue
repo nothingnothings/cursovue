a263ef3d40e73701de380057a08a77ceccd71920
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-04-08 14:54:25 -0300

    finished lesson 5

--- MODULO15-VUEX/projeto-com-vuex/src/App.vue
+++ MODULO15-VUEX/projeto-com-vuex/src/App.vue
@@ -3,23 +3,31 @@
     <!-- EXEMPLO DE UTILIZAÇÃO DO GLOBAL STORE DO VUEX... -->
     <!-- <h3>{{ $store.state.counter }}</h3> -->
     <TheCounter></TheCounter>
-    <button>Add 1</button>
+    <button @click="addOne">Add 1</button>
+    <ChangeCounter></ChangeCounter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from "./components/BaseContainer.vue";
 import TheCounter from "./components/TheCounter.vue";
+import ChangeCounter from "./components/ChangeCounter.vue";
 
 export default {
   components: {
     BaseContainer,
-    TheCounter
+    TheCounter,
+    ChangeCounter,
   },
 
   methods: {
     addOne() {
-      $store.state.counter++;
+      ///MANEIRA ERRADA/IMPRÓPRIA DE ATUALIZAR O STATE DO VUEX (sem mutations/actions)...
+      // $store.state.counter + 11; ///example of a possible mistype/mistake on updating the state..
+      
+      //atualize seu state ASSIM:
+      this.$store.commit("increment"); ///exemplo de USO DE MUTATION...
+  
     },
   },
 };
---
+++ MODULO15-VUEX/projeto-com-vuex/src/components/ChangeCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="addOne">Add 1</button>
+</template>
+
+
+<script>
+export default {
+  methods: {
+    addOne() {
+      //não atualize o store de seu vuex assim... use MUTATIONS, COM 'this.$store.commit('mutationName')'
+      //   this.$store.state.counter++;
+
+      this.$store.commit("increment"); ///exemplo de USO DE MUTATION...
+    },
+  },
+};
+</script>
