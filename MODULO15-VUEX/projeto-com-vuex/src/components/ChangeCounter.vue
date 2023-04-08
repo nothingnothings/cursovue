1ce331a1386ffa4318936160b2963de429596bb1
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-04-08 16:53:10 -0300

    finished lesson 10

--- MODULO15-VUEX/projeto-com-vuex/src/components/ChangeCounter.vue
+++ MODULO15-VUEX/projeto-com-vuex/src/components/ChangeCounter.vue
@@ -1,26 +1,33 @@
 <template>
-  <button @click="addOne">Add 1</button>
+  <div>
+    <button @click="increment">Add 1</button>
+    <button @click="incrementCustom(25)">Add 25</button>
+  </div>
 </template>
 
 
 <script>
+import { mapActions } from "vuex";
 export default {
   methods: {
-    addOne() {
-      //não atualize o store de seu vuex assim... use MUTATIONS, COM 'this.$store.commit('mutationName')'
-      //   this.$store.state.counter++;
+    // addOne() {
+    //não atualize o store de seu vuex assim... use MUTATIONS, COM 'this.$store.commit('mutationName')'
+    //   this.$store.state.counter++;
 
-      //   this.$store.commit("increment"); ///exemplo de USO DE MUTATION... (É QUASE A MANEIRA CORRETA...)
+    //   this.$store.commit("increment"); ///exemplo de USO DE MUTATION... (É QUASE A MANEIRA CORRETA...)
 
-      //A MANEIRA MAIS CORRETA DE ATUALIZAR O STATE É __ CHAMAR 1 ACTION/DISPARAR UMA ACTION, QUE ENTÃO FAZ COM QUE 1 COMMIT SEJA EXECUTADO, QUE ENTÃO DISPARA A MUTATION E FAZ O STATE SER ALTERADO...
+    //A MANEIRA MAIS CORRETA DE ATUALIZAR O STATE É __ CHAMAR 1 ACTION/DISPARAR UMA ACTION, QUE ENTÃO FAZ COM QUE 1 COMMIT SEJA EXECUTADO, QUE ENTÃO DISPARA A MUTATION E FAZ O STATE SER ALTERADO...
 
-      this.$store.dispatch("delayedIncrement");
+    //   PARA ABREVIAR/TER O ATALHO PARA ESSE METHOD, use 'mapActions'
+    //   this.$store.dispatch("delayedIncrement");
 
-      //   this.$store.commit({ //maneira ALTERNATIVA DE COMMITAR/CHAMAR 1 MUTATION...
-      //     type: "incrementCustom",
-      //     payload: 25,
-      //   });
-    },
+    //   this.$store.commit({ //maneira ALTERNATIVA DE COMMITAR/CHAMAR 1 MUTATION...
+    //     type: "incrementCustom",
+    //     payload: 25,
+    //   });
+    // },
+
+    ...mapActions(["increment", "incrementCustom"]), //shortcut ao call de ACTIONS manualmente, com 'mapActions':
   },
 };
 </script>
--- MODULO15-VUEX/projeto-com-vuex/src/components/TheCounter.vue
+++ MODULO15-VUEX/projeto-com-vuex/src/components/TheCounter.vue
@@ -1,14 +1,25 @@
 <template>
-  <h3>{{ counter }}</h3>
+  <!-- <h3>{{ counter }}</h3> -->
+  <h3>{{ finalCounter }}</h3>
 </template>
 
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   computed: {
-    counter() {
-      return $store.state.counter;
-    },
+    // counter() {
+    //sempre tente acessar seu store por meio de GETTERS, E NÃO DIRETAMENTE...
+    //   return this.$store.state.counter;
+
+    // SE DESEJAMOS ESCREVER MENOS CÓDIGO/TER UM SHORTCUT
+    // AOS NOSSOS GETTERS, PODEMOS USAR
+    // O METHOD DE 'mapGetters'...
+    //   return this.$store.getters.finalCounter;
+
+    // },
+
+    ...mapGetters(["finalCounter"]), //shortcut ao call de getters manualmente, com 'mapGetters':
   },
 };
 </script>
