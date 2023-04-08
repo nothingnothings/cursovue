20c2a5c1d6c6d44692ecdb3180bdf6b42ef47a14
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-04-08 12:32:22 -0300

    finished lesson 4

--- MODULO15-VUEX/projeto-com-vuex/src/App.vue
+++ MODULO15-VUEX/projeto-com-vuex/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <base-container title="Vuex">
-  <!-- EXEMPLO DE UTILIZAÇÃO DO GLOBAL STORE DO VUEX... -->
-    <h3>{{ $store.state.counter }}</h3>
+    <!-- EXEMPLO DE UTILIZAÇÃO DO GLOBAL STORE DO VUEX... -->
+    <!-- <h3>{{ $store.state.counter }}</h3> -->
+    <TheCounter></TheCounter>
     <button>Add 1</button>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from "./components/BaseContainer.vue";
+import TheCounter from "./components/TheCounter.vue";
 
 export default {
   components: {
     BaseContainer,
+    TheCounter
+  },
+
+  methods: {
+    addOne() {
+      $store.state.counter++;
+    },
   },
 };
 </script>
---
+++ MODULO15-VUEX/projeto-com-vuex/src/components/TheCounter.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3>{{ counter }}</h3>
+</template>
+
+
+<script>
+export default {
+  computed: {
+    counter() {
+      return $store.state.counter;
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
