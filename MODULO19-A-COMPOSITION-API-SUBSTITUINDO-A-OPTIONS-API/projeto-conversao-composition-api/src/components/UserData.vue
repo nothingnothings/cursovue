ed4bed00ff5e665eae8076eceafeb3c72a0230b0
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-06 12:06:47 -0300

    finished lesson 17

--- MODULO19-A-COMPOSITION-API-SUBSTITUINDO-A-OPTIONS-API/projeto-conversao-composition-api/src/components/UserData.vue
+++ MODULO19-A-COMPOSITION-API-SUBSTITUINDO-A-OPTIONS-API/projeto-conversao-composition-api/src/components/UserData.vue
@@ -6,6 +6,16 @@
 </template>
   <script>
 import { computed } from 'vue';
+
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue'; //// ESSES SÃO OS LIFECYCLE HOOKS DISPONÍVEIS, NA COMPOSITION API (beforeCreate e created NÃO EXISTEM, pq já são essencialmente a mesma coisa que 'setup()'...)
+
 export default {
   /// props: ['userName', 'age'],
 
@@ -15,17 +25,66 @@ export default {
     ///props vai segurar todos os props passados a esse component nosso...
     ///'context' é usado para coisas como CUSTOM EVENTS, por exemplo..
 
-
     //dentro de context, temos: 'attrs' (passthrough attributes passados a esse component)
-    // 'slots' --> é a data passada dentro dos slots desse component (se ele tiver algum slot) 
+    // 'slots' --> é a data passada dentro dos slots desse component (se ele tiver algum slot)
     // 'emit' --> é 1 function, MT IMPORTANTE, USADA PARA EMITIR DATA (exatamente como 'this.emit()')
-
-
 
     context.emit('exemploDeEventoCustom'); ////é assim que emitimos eventos, com a composition api..
     const uName = computed(() => {
       return props.firstName;
     });
+
+
+
+
+
+
+    onBeforeMount(
+      () => {
+        console.log('onBeforeMount')
+      }
+    )
+
+    onMounted(
+      () => {
+        console.log('onMounted')
+      }
+    )
+
+
+      onBeforeUpdate(
+        () => {
+          console.log('onBeforeUpdate')
+        }
+      )
+
+
+
+        onUpdated(
+          () => {
+            console.log('onUpdated')
+          }
+        )
+
+
+
+
+        onBeforeUnmount(
+          () => {
+            console.log('onBeforeUnmount')
+          }
+        )
+
+
+          onUnmounted( ///é chamado quando o component é removido do dom..
+            () => {
+              console.log('onUnmounted')
+            }
+          )
+
+
+
+
 
     return {
       userName: uName,
