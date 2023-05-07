de8eedc071c5b54e9be0ab4022350881e0504c73
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-06 21:14:26 -0300

    finsihed lesson 22

---
+++ MODULO19-A-COMPOSITION-API-SUBSTITUINDO-A-OPTIONS-API/projeto-routing-com-composition-api/src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import AllProducts from './pages/AllProducts.vue';
+import ProductDetails from './pages/ProductDetails.vue';
+import AddProduct from './pages/AddProduct.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/products' },
+    { path: '/products', component: AllProducts },
+    { path: '/products/:pid', component: ProductDetails,
+    //  props: true  /// É UMA DAS MANEIRAS DE OBTER COISAS COMO 'ROUTE QUERY PARAMS' na COMPOSITION API...
+    },
+    { path: '/products/add', component: AddProduct },
+  ],
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
---
+++ MODULO19-A-COMPOSITION-API-SUBSTITUINDO-A-OPTIONS-API/projeto-routing-com-composition-api/src/pages/ProductDetails.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <h2>{{ selectedProduct.title }}</h2>
+    <h3>${{ selectedProduct.price }}</h3>
+    <p>{{ selectedProduct.description }}</p>
+  </section>
+</template>
+
+<script>
+import { inject } from 'vue';
+
+export default {
+  props: ['pid'],
+
+  setup(props) {
+    const products = inject('products'); ////esses products advêm do component 'App.vue', onde estão armazenados...
+    // const title = ref('');
+    // const price = ref(null);
+    // const description = ref('');
+
+    const selectedProduct = products.value.find((product) => {
+      product.id === props.pid; ///'props.pid' --> é um value passado por meio do ROUTER ('props: true', lá no)
+    });
+
+    return { selectedProduct };
+  },
+};
+</script>
+
+
+<style scoped>
+section {
+  margin: 3rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
