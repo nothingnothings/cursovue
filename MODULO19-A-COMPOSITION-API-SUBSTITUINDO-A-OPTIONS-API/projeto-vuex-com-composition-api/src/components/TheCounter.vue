09b0bba4533155cc4ea7e78010650311a1bcf5be
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-06 21:41:26 -0300

    finished lesson 24

---
+++ MODULO19-A-COMPOSITION-API-SUBSTITUINDO-A-OPTIONS-API/projeto-vuex-com-composition-api/src/components/TheCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>{{ counter }}</h2>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+export default {
+  setup() {
+    const store = useStore();
+
+    // const counter = 0;
+
+    const counter = computed(() => {
+      return store.getters.counter; ///É ASSIM QUE GANHAMOS ACESSO à DATA ARMAZENADA NO NOSSO STORE...
+    });
+
+    return { counter };
+  },
+};
+</script>
