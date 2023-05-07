32394c3b570f840db0c50b6c8f3a9b25bee81042
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-06 21:30:53 -0300

    finished lesson 23

--- MODULO19-A-COMPOSITION-API-SUBSTITUINDO-A-OPTIONS-API/projeto-routing-com-composition-api/src/pages/ProductDetails.vue
+++ MODULO19-A-COMPOSITION-API-SUBSTITUINDO-A-OPTIONS-API/projeto-routing-com-composition-api/src/pages/ProductDetails.vue
@@ -8,18 +8,31 @@
 
 <script>
 import { inject } from 'vue';
+import { useRoute } from 'vue-router'; ///É UM HOOK USADO COM A COMPOSITION API (para ter tipo o 'this.$router' e o 'this.$route'...)
 
 export default {
   props: ['pid'],
 
   setup(props) {
+    const route = useRoute(); ////NOS DÁ ACESSO AO OBJECT 'route' do vue... --> será um REACTIVE OBJECT( o que é melhor)..
+
+    console.log(route);
+
     const products = inject('products'); ////esses products advêm do component 'App.vue', onde estão armazenados...
     // const title = ref('');
     // const price = ref(null);
     // const description = ref('');
 
+    //versão que USA OS 'props' DESSE COMPONENT 'PAGE' PARA CONSEGUIR OBTER OS DYNAMIC PARAMS DE NOSSA ROUTE...
+    // const selectedProduct = products.value.find((product) => {
+    //   product.id === props.pid; ///'props.pid' --> é um value passado por meio do ROUTER ('props: true', lá no)
+    // });
+
+
+    //versão que usa o 'useRoute' PARA CONSEGUIR OBTER OS DYNAMIC PARAMS DESSA ROUTE...
+
     const selectedProduct = products.value.find((product) => {
-      product.id === props.pid; ///'props.pid' --> é um value passado por meio do ROUTER ('props: true', lá no)
+      product.id === route.params.pid; ///'props.pid' --> é um value passado por meio do ROUTER ('props: true', lá no)
     });
 
     return { selectedProduct };
