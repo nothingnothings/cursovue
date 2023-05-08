399f1debe342024423de340e4d91120bb4720fdf
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-08 11:13:18 -0300

    finished lesson 10

---
+++ MODULO20-REUTILIZANDOFUNCIONALIDADE,MIXINS-E-CUSTOM-COMPOSITION-FUNCTIONS/projeto-custom-search-hook/hooks/search.js
@@ -0,0 +1,72 @@
+import { ref, watch, computed } from 'vue';
+
+export default function search(items, itemName) {
+  // este hook receberá uma propriedade de 'props' como parameter..
+  //data
+  const enteredSearchTerm = ref('');
+  const activeSearchTerm = ref('');
+  const sorting = ref(null);
+
+  //methods
+  const updateSearch = (val) => {
+    enteredSearchTerm.value = val;
+  };
+
+  const sort = (mode) => {
+    sorting.value = mode;
+  };
+  //watch
+
+  watch(enteredSearchTerm, (val) => {
+    setTimeout(() => {
+      if (val === enteredSearchTerm.value) {
+        activeSearchTerm.value = val;
+      }
+    }, 300);
+  });
+
+  //computed
+  const availableItems = computed(() => {
+    let filteredItems = [];
+    if (activeSearchTerm.value) {
+      filteredItems = items.filter((item) => {
+        if (itemName === 'user') {
+          return item.fullName.includes(activeSearchTerm.value);
+        }
+
+        if (itemName === 'project') {
+          return item.title.includes(activeSearchTerm.value);
+        }
+      });
+    } else if (items) {
+      filteredItems = items;
+    }
+    return filteredItems;
+  });
+
+  const displayedItems = computed(() => {
+    if (!sorting.value) {
+      return availableItems.value;
+    }
+    return availableItems.value.slice().sort((u1, u2) => {
+      if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
+        return 1;
+      } else if (sorting.value === 'asc') {
+        return -1;
+      } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+  });
+
+  return [
+    enteredSearchTerm,
+    activeSearchTerm,
+    availableItems,
+    displayedItems,
+    updateSearch,
+    sort,
+  ];
+}
---
+++ MODULO20-REUTILIZANDOFUNCIONALIDADE,MIXINS-E-CUSTOM-COMPOSITION-FUNCTIONS/projeto-custom-search-hook/src/components/projects/ProjectsList.vue
@@ -0,0 +1,156 @@
+<template>
+  <base-container v-if="user">
+    <h2>{{ user.fullName }}: Projects</h2>
+    <base-search
+      v-if="hasProjects"
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
+    <ul v-if="hasProjects">
+      <project-item
+        v-for="prj in availableProjects"
+        :key="prj.id"
+        :title="prj.title"
+      ></project-item>
+    </ul>
+    <h3 v-else>No projects found.</h3>
+  </base-container>
+  <base-container v-else>
+    <h3>No user selected.</h3>
+  </base-container>
+</template>
+
+<script>
+// import { watch, ref, computed, toRefs } from 'vue';
+import useSearch from '../../../hooks/search';
+import ProjectItem from './ProjectItem.vue';
+
+export default {
+  components: {
+    ProjectItem,
+  },
+  props: ['user'],
+
+  setup(props) {
+    // //methods
+    // const updateSearch = (val) => {
+    //   enteredSearchTerm.value = val;
+    // };
+
+    // //data
+    // const enteredSearchTerm = ref('');
+    // const activeSearchTerm = ref('');
+
+    // //computed
+    // const hasProjects = computed(() => {
+    //   return props.user.projects && availableProjects.value.length > 0;
+    // });
+
+    // const availableProjects = computed(() => {
+    //   if (activeSearchTerm.value) {
+    //     return props.user.projects.filter((prj) =>
+    //       prj.title.includes(activeSearchTerm.value)
+    //     );
+    //   }
+    //   return props.user.projects;
+    // });
+
+    // //watch
+    // watch(enteredSearchTerm, (val) => {
+    //   setTimeout(() => {
+    //     if (val === enteredSearchTerm.value) {
+    //       activeSearchTerm.value = val;
+    //     }
+    //   }, 300);
+    // });
+
+    // // watch(props.user, () => { ////NÃO PODEMOS FAZER WATCH DE 'user' EM SI, PQ O 'user' EM SI __ NAÕ É UM REACTIVE VALUE...
+    // // watch(props, () => {  ////como 'props' é um REACTIVE OBJECT, é possível fazer WATCH de seu value...
+    // //   enteredSearchTerm.value = '';
+    // // });
+
+    // // PORÉM, MELHOR DO QUE ISSO É __ FAZER WATCH DA REF DE 'user', que pode ser extraída por meio de 'toRefs' e OBJECT DESTRUCTURING, tipo assim:
+
+    // const { user } = toRefs(props);
+
+    // watch(user, () => {
+    //   enteredSearchTerm.value = '';
+    // });
+
+    // return {
+    //   enteredSearchTerm,
+    //   activeSearchTerm,
+    //   hasProjects,
+    //   availableProjects,
+    //   updateSearch,
+    // };
+
+    const [
+      enteredSearchTerm,
+      activeSearchTerm,
+      hasProjects,
+      availableProjects,
+      availableUsers,
+      displayedUsers,
+      sort,
+      updateSearch,
+    ] = useSearch(props.user.projects, 'project');
+
+    return {
+      enteredSearchTerm,
+      activeSearchTerm,
+      hasProjects,
+      availableProjects,
+      availableUsers,
+      displayedUsers,
+      sort,
+      updateSearch,
+    };
+  },
+
+  // data() {
+  //   return {
+  //     enteredSearchTerm: '',
+  //     activeSearchTerm: '',
+  //   };
+  // },
+  // computed: {
+  //   hasProjects() {
+  //     return this.user.projects && this.availableProjects.length > 0;
+  //   },
+  //   availableProjects() {
+  //     if (this.activeSearchTerm) {
+  //       return this.user.projects.filter((prj) =>
+  //         prj.title.includes(this.activeSearchTerm)
+  //       );
+  //     }
+  //     return this.user.projects;
+  //   },
+  // },
+  // methods: {
+  //   updateSearch(val) {
+  //     this.enteredSearchTerm = val;
+  //   },
+  // },
+  // watch: {
+  //   enteredSearchTerm(val) {
+  //     setTimeout(() => {
+  //       if (val === this.enteredSearchTerm) {
+  //         this.activeSearchTerm = val;
+  //       }
+  //     }, 300);
+  //   },
+  //   user() {
+  //     this.enteredSearchTerm = '';
+  //   },
+  // },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
